ecc3d65bc51e65a29fcb24593d4a00b2dd13f2eb
Varad <varaddeshmukh10june@gmail.com>
2022-03-04 12:57:46 +0530

    Added Passenger Counter Project

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let countEl=document.getElementById("count-el")
+let saveEl =document.getElementById("save-el") 	
+let count=0
+function increment(){
+	count=count+1
+	countEl.textContent =count
+	console.log(count)
+    
+}
+
+function save(){
+	let entry= count+" - "
+	saveEl.textContent+=entry
+	countEl.textContent=0
+	count=0
+}
